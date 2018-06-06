64424c46a40f5ae22ec84f3b85ded78a29e18d85
winni <bnhg093@hotmali.com>
2018-06-06 12:01:09 +0800

    Initial Commit

---
+++ HelloMyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloMyMap
+//
+//  Created by winni on 2018/6/6.
+//  Copyright © 2018年 winni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
